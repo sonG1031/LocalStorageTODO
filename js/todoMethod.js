99c4771bad0f52a66bac6e7a9ade2a9101e9503f
Minsub Song <minsub1031@gmail.com>
2023-04-12 11:31:08 +0900

    Delete Todo

--- js/todoMethod.js
+++ js/todoMethod.js
@@ -1,7 +1,7 @@
 const tblBody = document.getElementById("todoList");
 
 
-const add = (todo, id, save=false) => {
+const add = (todo, id, idx, save=false) => {
     if (todo == '') {
         alert("할 일을 입력해주세요.");
         return;
@@ -21,12 +21,14 @@ const add = (todo, id, save=false) => {
     
     const deleteBtn = document.createElement("button");
     deleteBtn.setAttribute('type', 'submit');
+    deleteBtn.setAttribute('data-idx', idx);
     deleteBtn.classList.add('btn');
     deleteBtn.classList.add('btn-danger');
     deleteBtn.innerHTML = "삭제";
 
     const finishedBtn = document.createElement("button");
     finishedBtn.setAttribute('type', 'submit');
+    finishedBtn.setAttribute('data-idx', idx);
     finishedBtn.classList.add('btn');
     finishedBtn.classList.add('btn-success');
     finishedBtn.classList.add('ms-1');
@@ -45,6 +47,7 @@ const add = (todo, id, save=false) => {
         for (let i = 0; i < todos.length; i++) {
             if (todos[i].id == id) {
                 todos[i].todos.push(todo);
+                break;
             }
         }
         localStorage.setItem("todos", JSON.stringify(todos));
@@ -54,12 +57,40 @@ const add = (todo, id, save=false) => {
 const addTodo = () => {
     let currentUser = JSON.parse(localStorage.getItem("currentUser"));
     const todo = document.getElementsByClassName("todo")[0];
-    add(todo.value, currentUser.id, true);
+    let todos = JSON.parse(localStorage.getItem("todos"));
+    let idx = 0;
+
+    for (let i = 0; i < todos.length; i++) {
+        if(currentUser.id == todos[i].id) {
+            idx = todos[i].todos.length;
+        }
+    }
+
+    add(todo.value, currentUser.id, idx, true);
     todo.value = "";
 }
 
+const remove = (id, idx) => {
+    let todos = JSON.parse(localStorage.getItem("todos"));
+
+    for(let i = 0; i < todos.length; i++) {
+        if(todos[i].id == id) {
+            todos[i].todos.splice(parseInt(idx), 1);
+        }
+    }
+    localStorage.setItem("todos", JSON.stringify(todos));
+    location.reload();
+};
+
 const removeTodo =() => {
-    
+    tblBody.addEventListener('click', function(event) {
+        let idx = event.target.dataset.idx;
+        console.log(typeof idx);
+        if(!idx) return;
+
+        let currentUser = JSON.parse(localStorage.getItem("currentUser"));
+        remove(currentUser.id, idx);
+    });
 };
 
 (()=>{
@@ -71,9 +102,11 @@ const removeTodo =() => {
     for (let i = 0; i < todos.length; i++) {
         if (todos[i].id == user.id) {
             for(let j = 0; j < todos[i].todos.length; j++) {
-                add(todos[i].todos[j], user.id);
+                add(todos[i].todos[j], user.id, j);
             }
             break;
         }
     }
+    removeTodo();
 })();
+
